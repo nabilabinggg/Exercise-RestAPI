cf2f3af3822173b8d910b69aa5d7e63178619d8e
nabila.ashfia <nabila.ashfia@gdn-commerce.com>
2022-07-13 10:17:02 +0700

    connect mongodb

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGO_URI, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
